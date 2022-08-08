44e77f4d587123489d82ee0787542639b5b7b353
Arpit <arpit@racloop.com>
2022-08-08 12:36:21 +0530

    Recipe - Repositories added

---
+++ recipe/src/main/java/com/springboot/recipe/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.recipe.repository;
+
+import com.springboot.recipe.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
